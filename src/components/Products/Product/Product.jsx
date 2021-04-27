b36f0efe4a5faa8f4ba9ac286f3bea58a7ff5843
Winston Chen <winstonchen.wm@gmail.com>
2021-04-26 23:41:05 -0400

    Product Component

---
+++ src/components/Products/Product/Product.jsx
@@ -0,0 +1,25 @@
+import { FormHelperText } from '@material-ui/core'
+import React from 'react'
+import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core'
+import { AddShoppingCart } from '@material-ui/icons'
+
+const Product = ({product}) => {
+    return (
+        <Card className={classes.root}>
+            <CardMedia className={classes.media} image='' title={product.name} />
+            <CardContent>
+                <div className={classes.cardContent}>
+                    <Typography variant="h5" gutterBottom>
+                        {product.name}
+                    </Typography>
+                    <Typography variant="h5">
+                        {product.price}
+                    </Typography>
+                </div>
+                <Typography variant="h2" color="textSecondary">{product.description}</Typography>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default Product
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import Grid from '@material-ui/core';
 
 const products = [
-    {id: 1, name: 'Book', description: 'Good book.'},
-    {id: 1, name: 'Book', description: 'Good book1.'},
+    {id: 1, name: 'Book', description: 'Good book.', price: '$5'},
+    {id: 1, name: 'Book', description: 'Good book1.', price: '$6'},
 ];
 
 const Products = () => {
@@ -11,7 +11,7 @@ const Products = () => {
         <Grid container justify="center" spacing={4}>
             {products.map((product) => (
                 <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-                    <Product />
+                    <Product product={product} />
                 </Grid>
             ))}
         </Grid>
